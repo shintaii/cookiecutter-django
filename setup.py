6c58521af40f5250d8afb61dcf92a17b9b850a64
Patrick Gerrits <info@gecab.nl>
2021-05-04 10:20:28 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,13 +21,13 @@ with open("README.rst") as readme_file:
     long_description = readme_file.read()
 
 setup(
-    name="cookiecutter-django",
+    name="cookiecutter-django-digitx",
     version=version,
     description="A Cookiecutter template for creating production-ready Django projects quickly.",
     long_description=long_description,
-    author="Daniel Roy Greenfeld",
-    author_email="pydanny@gmail.com",
-    url="https://github.com/pydanny/cookiecutter-django",
+    author="Patrick Gerrits",
+    author_email="patrick@digitx.nl",
+    url="https://github.com/shintaii/cookiecutter-django-digitx",
     packages=[],
     license="BSD",
     zip_safe=False,
